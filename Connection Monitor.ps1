df4fe4add710f2af16928cf2ac0a800d6f0654c7
MohammadGabr360 <60032462+MohammadGabr360@users.noreply.github.com>
2021-02-17 07:54:16 +0200

    Update Connection Monitor.ps1

--- Connection Monitor.ps1
+++ Connection Monitor.ps1
@@ -9,76 +9,92 @@ if($dest -eq ''){
     Write-Host '[*] Blank destination entered. Using google.com' -ForegroundColor Cyan
 }
 
-$prevping                    = 0
-$total_packets               = 0
-$latency_sum                 = 0
-$packets_lost                = 0
-$packetloss_reset_threshold  = 60
+$List = New-Object -TypeName "System.Collections.ArrayList"
 
-$prevping = (Test-Connection $dest -Count 1 -ErrorAction SilentlyContinue).ResponseTime
+$time_between_pings_milliseconds = 1000
+
+$ping_properties = @(
+    'Latency'
+    'LostPacket'
+)
+
+$Results_to_keep = 60
 
 while($true){
 
-    if($total_packets -eq $packetloss_reset_threshold){
-        $total_packets = 0
-        $packets_lost  = 0
-        $latency_sum   = 0
+    if($List.Count -ge $Results_to_keep){
+        $List.Remove($list[0])
     }
 
-    $total_packets += 1
+    $ping_object = @{} | Select-Object -Property $ping_properties
 
     try{
 
-        $ping               = Test-Connection $dest -Count 1
+        $ping = Test-Connection $dest -Count 1 | Select-Object -Property ProtocolAddress, ResponseTime, ResponseTimeToLive
+        $ping_object.LostPacket = $false
+        $ping_object.Latency = $ping.ResponseTime
+        $List.Add($ping_object) | Out-Null
+        if($List.Count -eq 0 -or ($List[$List.Count - 2]).LostPacket){
+            $Jitter = 0
+        }
+        else{
+            $prev_ping_latency = ($List[$List.Count - 2]).Latency
+            $Jitter  = [math]::abs($prev_ping_latency - $ping_object.Latency)
+        }
 
-        if    ($ping.ResponseTime -lt 100){$pingcolor        = 'Green' ; $sign = '[+]'}
-        elseif($ping.ResponseTime -lt 200){$pingcolor        = 'Yellow'; $sign = '[!]'}
-        else                              {$pingcolor        = 'Red'   ; $sign = '[-]'}
+        if($ping_object.Latency  -lt 100){
+            $pingColor = 'Green'
+            $Sign = '[+]'
+        }
+        elseif($ping_object.Latency  -lt 200){
+            $PingColor = 'Yellow'
+            $Sign = '[!]'
+        }
+        else{
+            $PingColor = 'Red'
+            $Sign = '[-]'
+        }
 
-        $jitter             = [math]::abs($prevping - $ping.ResponseTime)
+        if    ($Jitter -lt 10 ){$JitColor = 'Green' }
+        elseif($Jitter -lt 20 ){$JitColor = 'Yellow'}
+        else                   {$JitColor = 'Red'   }
 
-        if    ($jitter -lt 10)            {$jitcolor         = 'Green'                }
-        elseif($jitter -lt 20)            {$jitcolor         = 'Yellow'               }
-        else                              {$jitcolor         = 'Red'                  }
+        $average_latency = ($List.Latency | Measure-Object -Average | Select-Object -ExpandProperty Average) -as [int]
 
-        $average_latency    = $latency_sum/$total_packets          -as [int]
+        if    ($average_latency    -lt 100){$avgcolor = 'Green'                }
+        elseif($average_latency    -lt 200){$avgcolor = 'Yellow'               }
+        else                               {$avgcolor = 'Red'                  }
 
-        if    ($average_latency -lt 100)  {$avgcolor         = 'Green'                }
-        elseif($average_latency -lt 200)  {$avgcolor         = 'Yellow'               }
-        else                              {$avgcolor         = 'Red'                  }
+        $packetloss_percent = (($List | Where-Object {$_.LostPacket -eq $true} | Measure-Object | Select-Object -ExpandProperty count)/$List.count) * 100 -as [int]
 
-        $packetloss_percent = ($packets_lost/$total_packets) * 100 -as [int]
+        if    ($packetloss_percent -lt 10 ){$packetloss_color = 'Green'                }
+        elseif($packetloss_percent -lt 20 ){$packetloss_color = 'Yellow'               }
+        else                               {$packetloss_color = 'Red'                  }
 
-        if    ($packetloss_percent -lt 10){$packetloss_color = 'Green'                }
-        elseif($packetloss_percent -lt 20){$packetloss_color = 'Yellow'               }
-        else                              {$packetloss_color = 'Red'                  }
-
-        $prevping = $ping.ResponseTime
-        $latency_sum   += $ping.ResponseTime
-
-        Write-Host "$sign "                                                   -NoNewline -ForegroundColor $pingcolor 
-        Write-Host "IP Address: "                                             -NoNewline
-        Write-Host "$($ping.ProtocolAddress) | ".PadLeft(15,' ')              -NoNewline
-        Write-Host "Bytes: $($ping.BufferSize) | "                            -NoNewline
-        Write-Host "Latency: "                                                -NoNewline
-        Write-Host "$($ping.ResponseTime)".PadRight(4,' ')                    -NoNewline -ForegroundColor $pingcolor 
-        Write-Host " ms | "                                                   -NoNewline
-        Write-Host "TTL: $($ping.ResponseTimeToLive) hops | "                 -NoNewline
-        Write-Host "Jitter: "                                                 -NoNewline
-        Write-Host "$jitter ".PadRight(4,' ')                                 -NoNewline -ForegroundColor $jitcolor 
-        Write-Host "ms | "                                                    -NoNewline
-        Write-Host "Average Latency: "                                        -NoNewline
-        Write-Host "$average_latency".PadRight(4,' ')                         -NoNewline -ForegroundColor $avgcolor
-        Write-Host "ms | "                                                    -NoNewline
-        Write-Host "Packet Loss: "                                            -NoNewline
-        Write-Host "$packetloss_percent".PadRight(3,' ')                      -NoNewline -ForegroundColor $packetloss_color
+        Write-Host "$sign "                                       -NoNewline -ForegroundColor $pingcolor 
+        Write-Host "IP: "                                         -NoNewline
+        Write-Host "$($ping.ProtocolAddress) | ".PadLeft(15,' ')  -NoNewline
+        Write-Host "LAT: "                                        -NoNewline
+        Write-Host "$($ping.ResponseTime)".PadRight(4,' ')        -NoNewline -ForegroundColor $pingcolor 
+        Write-Host " ms | "                                       -NoNewline
+        Write-Host "TTL: $($ping.ResponseTimeToLive) | "          -NoNewline
+        Write-Host "JITT: "                                       -NoNewline
+        Write-Host "$jitter ".PadRight(4,' ')                     -NoNewline -ForegroundColor $jitcolor 
+        Write-Host "ms | "                                        -NoNewline
+        Write-Host "AVG: "                                        -NoNewline
+        Write-Host "$average_latency".PadRight(4,' ')             -NoNewline -ForegroundColor $avgcolor
+        Write-Host "ms | "                                        -NoNewline
+        Write-Host "LOSS: "                                       -NoNewline
+        Write-Host "$packetloss_percent".PadRight(3,' ')          -NoNewline -ForegroundColor $packetloss_color
         Write-Host "%"
     }
     catch{
-        $sign = '[x]'
+        $ping_object.LostPacket = $true
+        $List.Add($ping_object) | Out-Null
+        $Sign = '[x]'
         Write-Host "$sign Request timed out" -ForegroundColor Red
-        $packets_lost += 1
+        Write-Host $Error[0]
     }
 
-    sleep 1.12
+    Start-Sleep -Milliseconds $time_between_pings_milliseconds
 }
