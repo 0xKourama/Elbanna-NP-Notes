b584ae5245356879f3dfd94c43b823e7db0f5be8
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-02-23 22:05:40 +0200

    fix errors

--- Ping-Sweep.ps1
+++ Ping-Sweep.ps1
@@ -1,1 +1,1 @@
-1..254 | % {$list += "192.168.11.$_"}; Test-Connection -ComputerName $list -Count 1 -AsJob | Wait-Job | Receive-Job | ? {$null -ne $_.responsetime -or $null -ne $_.ipv4address} | Select -ExpandProperty Address
+$list = @(); 1..254 | % {$list += "192.168.1.$_"}; Test-Connection -ComputerName $list -Count 1 -AsJob | Wait-Job | Receive-Job | ? {$null -ne $_.responsetime -or $null -ne $_.ipv4address} | Select -ExpandProperty Address
