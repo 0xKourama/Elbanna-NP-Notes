8b9ef21187e65258111353bfc42173743a980730
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-03-03 00:11:43 +0200

    animation play

--- Animation.ps1
+++ Animation.ps1
@@ -3,18 +3,19 @@ $Remaining_Minutes = $Work_duration_minutes
 1..$Work_duration_minutes | ForEach-Object {
     Clear-Host
 
-    $border = '+' + ('-' * $Work_duration_minutes * 2) + '+'
+    $border1 = ' ╭' + ('━' * $Work_duration_minutes * 2) + '╮'
+    $border2 = ' ╰' + ('━' * $Work_duration_minutes * 2) + '╯'
 
     $upper = '\\' * $_
 
     $lower = '//' * $_
-    Write-Host $border
+    Write-Host $border1
     if    ($_ -le 10){$color = 'Red'   }
     elseif($_ -le 20){$color = 'Yellow'}
     else             {$color = 'Green' }
-    Write-Host ' \' -NoNewline; Write-Host $upper.PadRight($upper.Length + (($border.Length - 2) - $upper.Length)) -ForegroundColor $color -NoNewline; Write-Host '\'
-    Write-Host ' /' -NoNewline; Write-Host $lower.PadRight($upper.Length + (($border.Length - 2) - $upper.Length)) -ForegroundColor $color -NoNewline; Write-Host '/'
-    Write-Host $border
+    Write-Host ' \' -NoNewline; Write-Host $upper.PadRight($upper.Length + (($border1.Length - 2) - $upper.Length)) -ForegroundColor $color -NoNewline; Write-Host '\'
+    Write-Host ' /' -NoNewline; Write-Host $lower.PadRight($upper.Length + (($border1.Length - 2) - $upper.Length)) -ForegroundColor $color -NoNewline; Write-Host '/'
+    Write-Host $border2
     
 
     $Remaining_Minutes--
