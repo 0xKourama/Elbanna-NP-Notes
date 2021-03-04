1d2bfb0e3e777ad30173e37a3f68b510334cdc0a
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-03-04 07:40:19 +0200

    a little better

--- Invoke-PingSweep.ps1
+++ Invoke-PingSweep.ps1
@@ -1,1 +1,9 @@
-$list = @(); 1..254 | % {$list += "192.168.1.$_"}; Test-Connection -ComputerName $list -Count 1 -AsJob | Wait-Job | Receive-Job | ? {$null -ne $_.responsetime -or $null -ne $_.ipv4address} | Select -ExpandProperty Address
+param($Subnet)
+if(!$Subnet){Write-Host "[!] Usage: Invoke-PingSweep -Subnet 192.168.1" -ForegroundColor Yellow}
+else{
+    Write-Host "[*] Sweeping the $Subnet subnet" -ForegroundColor Cyan
+    Test-Connection -ComputerName (1..254 | ForEach-Object {"$Subnet.$_"}) -Count 1 -AsJob | 
+    Receive-Job -Wait | Where-Object {$_.StatusCode -eq 0} |
+    ForEach-Object {Write-Host "[+] $($_.Address) up" -ForegroundColor Green}
+    Write-Host "[*] Sweep finished" -ForegroundColor Cyan
+}
