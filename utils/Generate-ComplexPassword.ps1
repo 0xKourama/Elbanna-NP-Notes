7647c96683104f3327cd043cb3770397dc66d571
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-03-05 19:30:21 +0200

    add count feature

--- utils/Generate-ComplexPassword.ps1
+++ utils/Generate-ComplexPassword.ps1
@@ -1,4 +1,6 @@
-﻿#arrays to randomize from
+﻿param($Count)
+
+#arrays to randomize from
 $Alphabet      = 'abcdefghijklmnopqrstuvwxyz'
 $Chars_lower   = $Alphabet.ToCharArray()
 $Chars_Upper   = $Alphabet.ToUpper().ToCharArray()
@@ -9,32 +11,34 @@ $Special_Chars = '!@#$%^&*'.ToCharArray()
 $Password_length_Min = 15
 $Password_length_Max = 20
 
-#generate a random length
-$Random_length = Get-Random -Minimum $Password_length_Min -Maximum ($Password_length_Max + 1)
+1..$Count | ForEach-Object {
+    #generate a random length
+    $Random_length = Get-Random -Minimum $Password_length_Min -Maximum ($Password_length_Max + 1)
 
-#initialize password as an array
-$password = @()
+    #initialize password as an array
+    $password = @()
 
-1..$Random_length | % {
+    1..$Random_length | % {
 
-    #for the first 4 iterations, add one of each array i.e. add one lowercase, one uppercase, one number & one special character
-    #to the password so it conforms to password complexity rules
-    #then start selecting a random character to add to password
-    if($_ -lt 5){$Random_array = $_                              }
-    else        {$Random_array = Get-Random -Minimum 1 -Maximum 5}
+        #for the first 4 iterations, add one of each array i.e. add one lowercase, one uppercase, one number & one special character
+        #to the password so it conforms to password complexity rules
+        #then start selecting a random character to add to password
+        if($_ -lt 5){$Random_array = $_                              }
+        else        {$Random_array = Get-Random -Minimum 1 -Maximum 5}
 
-    switch($Random_array){
-        1{$Chosen_array = $Chars_Lower  }
-        2{$Chosen_array = $Chars_Upper  }
-        3{$Chosen_array = $Numbers      }
-        4{$Chosen_array = $Special_Chars}
+        switch($Random_array){
+            1{$Chosen_array = $Chars_Lower  }
+            2{$Chosen_array = $Chars_Upper  }
+            3{$Chosen_array = $Numbers      }
+            4{$Chosen_array = $Special_Chars}
+        }
+
+        $password += $Chosen_array[$(Get-Random -Minimum 0 -Maximum $Chosen_array.Count)]
     }
 
-    $password += $Chosen_array[$(Get-Random -Minimum 0 -Maximum $Chosen_array.Count)]
+    #shuffle the final password and turn it into a string
+    $password = ($password | Sort-Object {Get-Random}) -join ''
+
+    write-host 'Password: ' -NoNewline
+    write-host $password -ForegroundColor Green
 }
-
-#shuffle the final password and turn it into a string
-$password = ($password | Sort-Object {Get-Random}) -join ''
-
-write-host 'Password: ' -NoNewline
-write-host $password -ForegroundColor Green
