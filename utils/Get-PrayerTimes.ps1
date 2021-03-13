2435d7a457bd356cae73b044a6aa2fc05396dba7
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-03-13 20:41:15 +0200

    clean up script

--- utils/Get-PrayerTimes.ps1
+++ utils/Get-PrayerTimes.ps1
@@ -1,18 +1,25 @@
 $ProgressPreference = 'SilentlyContinue'
+
 $url = 'https://www.islamicfinder.org/world/egypt/360630/cairo-prayer-times/'
+
 $response = Invoke-WebRequest $url
+
 $matches = $response.content | Select-String -Pattern "prayername.*>(.*)<.*`n.*prayertime.*>(.*)<" -AllMatches | Select-Object -ExpandProperty matches
+
 class PrayerObject {
     [string]$Prayer
-    #[datetime]$Time
     [string]$Time
+    #[datetime]$Time
 }
+
 $PrayerTimes = @()
+
 0..5 | ForEach-Object {
     $PrayerTimes += [prayerObject]@{
         Prayer = $matches[$_].Groups[1].Value.trim()
-        #Time   = [datetime]::parseexact($matches[$_].Groups[2], 'hh:mm tt', $null)
         Time   = $matches[$_].Groups[2].Value.trim()
+        #Time   = [datetime]::parseexact($matches[$_].Groups[2], 'hh:mm tt', $null)
     }
 }
+
 $PrayerTimes
