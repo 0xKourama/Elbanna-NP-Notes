25ffa0515168e74a6dedad4b454b32adbcf27ae3
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-03-14 07:17:33 +0200

    error handling added

--- utils/Get-PrayerTimes.ps1
+++ utils/Get-PrayerTimes.ps1
@@ -1,25 +1,31 @@
 $ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
 
 $url = 'https://www.islamicfinder.org/world/egypt/360630/cairo-prayer-times/'
 
-$response = Invoke-WebRequest $url
+try{
+    $response = Invoke-WebRequest $url
 
-$matches = $response.content | Select-String -Pattern "prayername.*>(.*)<.*`n.*prayertime.*>(.*)<" -AllMatches | Select-Object -ExpandProperty matches
+    $matches = $response.content | Select-String -Pattern "prayername.*>(.*)<.*`n.*prayertime.*>(.*)<" -AllMatches | Select-Object -ExpandProperty matches
 
-class PrayerObject {
-    [string]$Prayer
-    [string]$Time
-    #[datetime]$Time
-}
-
-$PrayerTimes = @()
-
-0..5 | ForEach-Object {
-    $PrayerTimes += [prayerObject]@{
-        Prayer = $matches[$_].Groups[1].Value.trim()
-        Time   = $matches[$_].Groups[2].Value.trim()
-        #Time   = [datetime]::parseexact($matches[$_].Groups[2], 'hh:mm tt', $null)
+    class PrayerObject {
+        [string]$Prayer
+        [string]$Time
+        #[datetime]$Time
     }
-}
 
-$PrayerTimes
+    $PrayerTimes = @()
+
+    0..5 | ForEach-Object {
+        $PrayerTimes += [prayerObject]@{
+            Prayer = $matches[$_].Groups[1].Value.trim()
+            Time   = $matches[$_].Groups[2].Value.trim()
+            #Time   = [datetime]::parseexact($matches[$_].Groups[2], 'hh:mm tt', $null)
+        }
+    }
+
+    Write-Output $PrayerTimes
+}
+catch{
+    Write-Host "[!] An error occured: $_" -ForegroundColor Yellow
+}
