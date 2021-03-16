e5dfa40550d0ab8bee01bec459cd17ed48078976
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-03-16 22:24:42 +0200

    way sexier network perfmon

--- work/Test-NetworkBandwidth.ps1
+++ work/Test-NetworkBandwidth.ps1
@@ -1,36 +1,79 @@
-Param(
-    $Executable_location,
-    $OutputFile
-)
+$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Inquire'
 
-$ErrorActionPreference = 'Stop'
+$source_executable_folder = 'C:\Users\GabrUrgent\iperf\*'
+$Destination_executable_folder = 'C:\Users\GabrUrgent\iperf\'
 
-$ADComputers = Get-ADComputer -Filter * -Properties IPV4Address,DNSHostName | Where-Object {$_.IPV4Address} | Select-Object -ExpandProperty DNSHostName
+ipconfig /flushdns | Out-Null
 
-$Online_Hosts = Test-Connection -ComputerName $ADComputers -Count 1 -AsJob | Wait-Job | Receive-Job | Where-Object {$_.StatusCode -eq 0} | Select-Object -ExpandProperty Address
+if($?){Write-Host -ForeGroundColor Green '[+] DNS Cache Cleared'}
 
-$Online_Hosts | ForEach-Object {
-    if((Test-Path "\\$_\c$\Windows\Temp") -eq $false){
-        Copy-Item -Path $Executable_location -Destination "\\$_\c$\Windows\Temp" -PassThru
+$ADComputers = Get-ADComputer -Filter * -Properties IPV4Address | Where-Object {$_.IPV4Address} | Select-Object -ExpandProperty Name
+
+Write-Host  -ForegroundColor Cyan "[*] Attempting to start PowerShell sessions with $($ADComputers.Count) AD Computers.."
+
+$Sessions = New-PSSession -ComputerName $ADComputers -ErrorAction SilentlyContinue
+
+Write-Host  -ForegroundColor Green "[+] $($Sessions.Count) PowerShell sessions are now active"
+
+Write-Host  -ForegroundColor Cyan "[*] Starting copy to computers in session"
+
+foreach($ActiveSession in ($Sessions | Where-Object {$_.State -eq 'Opened' -and $_.Availability -eq 'Available' })){
+    Copy-Item $source_executable_folder -ToSession $ActiveSession `
+                                        -Destination $destination_executable_folder `
+                                        -Recurse -Force -ErrorAction SilentlyContinue
+    if($?){Write-Host -ForegroundColor Green "[+] [COPY SUCCESS] $($ActiveSession.ComputerName)"}
+    else{
+        Write-Host -ForegroundColor Red "[-] [COPY FAILURE] $($ActiveSession.ComputerName)"
+        Remove-PSSession -ComputerName $ActiveSession.ComputerName
     }
 }
 
-$Listener_Verbose_Code = {
-    $OutFileHost = $args[0]
-    $OutputFile = $args[1] -replace ':','$'
-    Write-Host "[*] Listener started on $env:COMPUTERNAME" -ForegroundColor Cyan
-    Write-Host "[*] writing output to \\$OutFileHost\$OutputFile" -ForegroundColor Cyan
+pause
+
+#populating test segments
+$Test_Segment_list = @()
+
+$ActiveSessions = Get-PSSession
+
+Write-Host -ForegroundColor Cyan "[*] Populating test segment list for $($ActiveSessions.Count) computers"
+
+foreach($Source in $Sessions.ComputerName){
+    foreach($Destination in $Sessions.ComputerName){
+        if($Source -ne $Destination -and `
+           $Test_Segment_list -notcontains "$Source<-->$Destination" -and `
+           $Test_Segment_list -notcontains "$Destination<-->$Source"
+        ){
+            Write-Host -NoNewline '['
+            Write-Host -NoNewline -ForegroundColor Green "$Source"
+            Write-Host -NoNewline ']'
+            Write-Host -NoNewline ' <--> '
+            Write-Host -NoNewline '['
+            Write-Host -NoNewline -ForegroundColor Red "$Destination"
+            Write-Host ']'
+
+            $Test_Segment_list += "$Source<-->$Destination"
+        }
+    }
 }
 
-$Listener_Code = {
-    C:\Windows\Temp\iperf3.exe -s # >> \\$OutFileHost\$OutputFile
+Write-Host -ForegroundColor Cyan "[*] $($Test_Segment_list.count) unique network segments found"
+
+pause
+
+$Test_Segments = @()
+
+foreach($entry in $Test_Segment_list){
+    $Obj = @{} | Select-Object -Property Source, Destination
+    $Obj.Source = ($entry -split '<-->')[0]
+    $Obj.Destination = ($entry -split '<-->')[1]
+    $Test_Segments += $Obj
 }
 
 $Connector_Code = {
     $Server = $args[0]
-    Write-Host "[*] Testing bandwidth to $Server from $env:COMPUTERNAME" -ForegroundColor Cyan
 
-    C:\Windows\Temp\iperf3.exe -c $Server |
+    $result = c:\users\gabrurgent\iperf\iperf3.exe -c $Server |
     Select-Object -Skip 3 -First 10 |
     Select-String -Pattern '(\d{1,3}.\d) MBytes' -AllMatches |
     Select-Object -ExpandProperty matches |
@@ -40,29 +83,49 @@ $Connector_Code = {
     Measure-Object -Average |
     Select-Object -ExpandProperty average
 
-    Write-Host "[+] Finished testing to $Server from $env:COMPUTERNAME" -ForegroundColor Green
+    Write-Host -NoNewline '['
+    Write-Host -NoNewline -ForegroundColor Cyan $env:COMPUTERNAME
+    Write-Host -NoNewline '] <---'
+    Write-Host -NoNewline "[$result MBytes/Second]"
+    Write-Host -NoNewline '---> ['
+    Write-Host -NoNewline -ForegroundColor Red $Server
+    Write-Host ']'
+
+    Write-Output $result
 }
 
+$ResultArray = @()
+
+<#
 foreach($Host1 in $Online_Hosts){
-    
-    $Listener_Session = New-PSSession -ComputerName $Host1
 
-    Invoke-Command -Session $Listener_Session -ScriptBlock $Listener_Verbose_Code -ArgumentList $env:COMPUTERNAME, $OutputFile
+    try{
+        $Listener_Session = New-PSSession -ComputerName $Host1
+        Invoke-Command -Session $Listener_Session -ScriptBlock {Write-Host "[*] Listener started on $env:COMPUTERNAME" -ForegroundColor Cyan}
+        $Listener_job = Invoke-Command -Session $Listener_Session -ScriptBlock {\\frank-rodc-01\c$\users\gabrurgent\iperf\iperf3.exe -s} -AsJob
 
-    $Listener_Job = Invoke-Command -Session $Listener_Session -ScriptBlock $Listener_Code -AsJob
+        foreach($Host2 in $Online_Hosts){
+            if($Host2 -ne $Host1){
+                $obj = @{} | Select-Object -Property Source, Destination, AverageTransferSpeedMB
 
-    Start-Sleep -Seconds 1
-    
-    foreach($Host2 in $Online_Hosts){
-        if($Host2 -ne $Host1){
-            $result = Invoke-Command -ComputerName $Host2 -ScriptBlock $Connector_Code -ArgumentList $Host1
+                $obj.Source      = $Host2
+                $obj.Destination = $Host1
+                $obj.AverageTransferSpeedMB = Invoke-Command -ComputerName $Host2 -ScriptBlock $Connector_Code
+
+                $ResultArray += $obj
+                Write-Output $obj | Format-Table -AutoSize
+            }
         }
+
+        Write-Host "[*] Stopping Listener on $Host1" -ForegroundColor Cyan
+        $Listener_Job | Stop-Job
+        $Listener_Job | Remove-Job
+        $Listener_Session | Remove-PSSession
+        Write-Host "[+] Listener stopped on $Host1" -ForegroundColor Green
     }
-
-    Write-Host "[*] Stopping Listener on $Host1" -ForegroundColor Cyan
-    $Listener_Job | Stop-Job
-    $Listener_Job | Remove-Job
-
-    $Listener_Session | Remove-PSSession
-    Write-Host "[+] Listener stopped on $Host1" -ForegroundColor Green
+    catch{
+        Write-Host "[-] An error has occured: $_" -ForegroundColor Red
+    }
 }
+$ResultArray | Format-Table -AutoSize
+#>
