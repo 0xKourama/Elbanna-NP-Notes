99be24a74d9d379fd32c0a096100707fe824a443
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-03-16 23:30:56 +0200

    more quotes

--- Show-ChargeBenefits.ps1
+++ Show-ChargeBenefits.ps1
@@ -1,10 +1,11 @@
 $Charge_benefits = @(
-    'Development of male growth and masculine characteristics]'
-    'Healthy heart and blood'
-    'Less fat, more muscle, deeper voice'
-    'Stronger bones'
-    'Better verbal memory, spatial abilities, or mathematical reasoning'
-    'Better libido'
-    'Improved mood'
+    'Win [Gods approval]'
+    'Development of [male growth] and [masculine characteristics]'
+    'Healthy [heart] and [blood]'
+    '[Less fat], [more muscle] and a [deeper voice]'
+    '[Stronger bones]'
+    'Better [verbal memory], [spatial abilities] and [mathematical reasoning]'
+    'Better [libido]'
+    'Improved [mood]'
 )
 $Charge_benefits | ForEach-Object {Write-Host -ForegroundColor DarkGreen "[+] $_"}
