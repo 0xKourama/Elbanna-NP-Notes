6c0e246440e071ecb4735e791e4673c21cfacb70
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-03-23 22:30:51 +0200

    finish work and sleep

--- work/Analyze-Header.ps1
+++ work/Analyze-Header.ps1
@@ -1,52 +1,54 @@
-$mail_path = 'C:\Users\admin\Desktop\Test Messages\*'
+$mail_path = "$env:USERPROFILE\Desktop\Test Messages\Headers"
 
-$ol = New-Object -ComObject Outlook.Application
+#$ol = New-Object -ComObject Outlook.Application
 
 foreach ($email in (Get-ChildItem $mail_path -Recurse | Select-Object -ExpandProperty fullname)){
 
     Write-Host "`n[*] Performing header analysis for $email`n" -ForegroundColor Yellow
-
+    Clear-Variable -Name Sender, SenderData, Recipient, Subject, headers
+    <#
     $msg = $ol.CreateItemFromTemplate("$email")
     $headers = $msg.PropertyAccessor.GetProperty("http://schemas.microsoft.com/mapi/proptag/0x007D001E")
+    #>
+
+    $headers = Get-Content $email
 
     $headers
 
     pause
 
     $Sender = (
-        $headers | Select-String -Pattern "From: .*\s+<(.*)>" -AllMatches |
+        $headers | Select-String -Pattern "^From:.*\s+(\S+@\S+)" -AllMatches |
         Select-Object -ExpandProperty matches |
         Select-Object -ExpandProperty groups
-    )[1].value
+    )[1].value -replace "<" -replace ">"
 
     $Recipient = (
-        $headers | Select-String -Pattern "To: <?(.*)>?" -AllMatches |
+        $headers | Select-String -Pattern "^To:.*\s+(\S+@\S+)" -AllMatches |  #^To: ((\S+\,\s+)*\S+)
         Select-Object -ExpandProperty matches |
         Select-Object -ExpandProperty groups
-    )[1].value
+    )[1].value -replace "<" -replace ">"
 
     $Subject = (
-        $headers | Select-String -Pattern "Subject: (.*)" -AllMatches |
+        $headers | Select-String -Pattern "^Subject: (.*)" -AllMatches |
         Select-Object -ExpandProperty matches |
         Select-Object -ExpandProperty groups    
     )[1].value
 
-    $SenderIP = (
-        $headers | Select-String -Pattern "Received: from .*[\(\[](.*)[\)\]]" -AllMatches |
+    $SenderData = (
+        $headers | Select-String -Pattern "^Received: from .*\((.*)\)" -AllMatches |
         Select-Object -ExpandProperty matches -Last 1 |
         Select-Object -ExpandProperty groups -Last 1
-    )[1].value -replace "]"
+    )[1].Value
 
-    Write-Host -NoNewline "Sender   : "
+    Write-Host -NoNewline "Sender      : "
     Write-Host -ForegroundColor Green $Sender
-    Write-Host -NoNewline "SenderIP : " 
-    Write-Host -ForegroundColor Cyan $SenderIP
-    Write-Host -NoNewline "Recipient: "
+    Write-Host -NoNewline "Sender Data : " 
+    Write-Host -ForegroundColor Cyan $SenderData
+    Write-Host -NoNewline "Recipient   : "
     Write-Host -ForegroundColor Magenta $Recipient
-    Write-Host -NoNewline "Subject  : "
+    Write-Host -NoNewline "Subject     : "
     Write-Host -ForegroundColor Yellow $Subject
-
-    Clear-Variable -Name Sender, SenderIP, Recipient, Subject, msg, headers
 
     pause
 }
