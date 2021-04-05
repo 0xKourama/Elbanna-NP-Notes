fabf87f77ac28f4a88bb6f6ee3f4cd17df9e2caa
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-04-05 18:41:15 +0200

    sick time module inshallah

--- powereye/development/11 - TimeCheck-Module.ps1
+++ powereye/development/11 - TimeCheck-Module.ps1
@@ -1,18 +1,25 @@
+Write-Host -ForegroundColor Cyan '[*] Testing connectivity'
 $Online = Get-ADComputer -Filter * | Select-Object -Property @{name = 'ComputerName'; Expression = {$_.name}} | 
           Test-Connection -Count 1 -AsJob | Receive-job -Wait | Where-Object {$_.statuscode -eq 0} | Select-Object -ExpandProperty Address
-$Script = {
-    $PDCTime = $args[0]
-    $Date = Get-Date
-    $Obj = New-Object -TypeName PSObject -Property @{
-        ComputerName     = $env:COMPUTERNAME
-        Date             = $Date
-        PDCTimeOffsetMS  = [math]::Abs((New-TimeSpan -Start $PDCTime -End $Date).TotalMilliseconds)
-    }
-    Write-Output $Obj
-}
+Write-Host -ForegroundColor Cyan "[*] $($Online.Count) Online computers found"
+
+Write-Host -ForegroundColor Cyan '[*] Starting sessions with computers'
+
 $Sessions = New-PSSession -ComputerName $Online -ErrorAction SilentlyContinue
 
-$PDCTime = Invoke-Command -ComputerName  -ScriptBlock {Get-Date} |
-        Select-Object -Property * -ExcludeProperty PSShowComputerName, RunSpaceID, PSComputerName
+Write-Host -ForegroundColor Cyan '[*] Collecting time results'
 
-$Results = Invoke-Command -Session $Sessions -ScriptBlock $Script -ArgumentList $PDCTime -ErrorAction SilentlyContinue
+$Results = Invoke-Command -Session $Sessions -ScriptBlock {Get-Date} -ErrorAction SilentlyContinue
+
+$PDCTime = $Results | Where-Object {$_.PSComputerName -eq "$((Get-ADDomain).PDCEmulator -replace ".$env:USERDNSDOMAIN" )"}
+
+$Results | Foreach-Object {
+    Add-Member -InputObject $_ -Type NoteProperty -Name PDCTimeOffsetS -Value ([math]::Round((New-TimeSpan -Start $_ -End $PDCTime).TotalSeconds, 2))
+    Add-Member -InputObject $_ -Type NoteProperty -Name PDCTimeOffsetMS -Value ((New-TimeSpan -Start $_ -End $PDCTime).TotalMilliseconds -as [int])
+}
+
+$Results | Select-Object -Property @{n='ComputerName';e={$_.PSComputerName}}, PDCTimeOffsetS, PDCTimeOffsetMS | Format-Table -AutoSize
+
+Clear-Variable -Name Results
+
+Remove-PSSession -Session $Sessions
