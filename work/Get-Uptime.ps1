162168344cc1e6fc36894eca816e13030fb919b3
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-04-05 07:45:28 +0200

    clean up unneeded scripts

--- work/Get-Uptime.ps1
+++
@@ -1,5 +0,0 @@
-param($ComputerName)
-Invoke-Command -ComputerName $ComputerName -ScriptBlock {
-    $uptime = New-TimeSpan -Start ([Management.ManagementDatetimeConverter]::ToDateTime((Get-WmiObject -Query "SELECT LastBootUpTime FROM Win32_OperatingSystem").LastBootupTime)) -End (get-date)
-    "$($env:computername): $($uptime.days) Days $($uptime.hours) hours $($uptime.minutes) minutes"
-} -ErrorAction SilentlyContinue
