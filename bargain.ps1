649e453f4de6553aacaa3e00f8017b7e47bd9838
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-04-25 23:44:26 +0200

    charge increase

--- bargain.ps1
+++ bargain.ps1
@@ -1,5 +1,16 @@
-Param([int]$Final)
-Write-Host -ForegroundColor Green   "[1] $($Final*0.65 -as [int])"
-Write-Host -ForegroundColor Yellow  "[2] $($Final*0.85 -as [int])"
-Write-Host -ForegroundColor Red     "[3] $($Final*0.95 -as [int])"
-Write-Host -ForegroundColor Magenta "[*] $([math]::round($Final*0.98,2))"
+Param(
+    [int]$Final,
+    [int]$Meters
+)
+Write-Host -NoNewline -ForegroundColor Green  ($Final*0.65 -as [int])
+Write-Host -NoNewline ' > '
+Write-Host -NoNewline -ForegroundColor Cyan   ($Final*0.85 -as [int])
+Write-Host -NoNewline ' > '
+Write-Host -NoNewline -ForegroundColor Yellow ($Final*0.95 -as [int])
+Write-Host -NoNewline ' > '
+Write-Host -ForegroundColor Red    ([math]::round($Final*0.98,2))
+Write-Host "Per meter      : $([Math]::Round($Final/$Meters,2))"
+Write-Host "Initial        : $($Final*0.2)"
+Write-Host "Final          : $($Final*0.1)"
+Write-Host "Monthly        : $([Math]::Round(($Final*0.7)/72,2))"
+Write-Host "Before Discount: $($Final*1.08 -as [int])"
