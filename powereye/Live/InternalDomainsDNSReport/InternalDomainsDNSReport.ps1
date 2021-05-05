92215d06e3d8ca34df7106241c4a1993a80cc4e9
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-05-05 14:53:24 +0200

    finish work and go home

--- powereye/Live/InternalDomainsDNSReport/InternalDomainsDNSReport.ps1
+++ powereye/Live/InternalDomainsDNSReport/InternalDomainsDNSReport.ps1
@@ -40,7 +40,7 @@ $Csv.Domain | ForEach-Object {
 
     try{
         #get MX records
-        $MX_Query  = Resolve-DnsName -Type MX -Name $Domain -DnsOnly | Select-Object -Property Name, NameExchange
+        $MX_Query  = Resolve-DnsName -Type MX -Name $Domain | Select-Object -Property Name, NameExchange
 
         if($MX_Query.NameExchange){
             foreach($MX_Result in $MX_Query){
@@ -64,11 +64,11 @@ $Csv.Domain | ForEach-Object {
         }
     }
     catch{
-        $Obj.MXError = $_
+        $Obj.MXError = $_ -replace "$Domain : "
     }
     try{
         #get TXT records
-        $TXT_Query  = Resolve-DnsName -Type TXT -Name $Domain -DnsOnly | Select-Object -Property Name, Strings
+        $TXT_Query  = Resolve-DnsName -Type TXT -Name $Domain | Select-Object -Property Name, Strings
 
         if($TXT_Query){
             foreach($TXT_Result in $TXT_Query){
@@ -81,11 +81,11 @@ $Csv.Domain | ForEach-Object {
         }        
     }
     catch{
-        $Obj.TXTError = $_    
+        $Obj.TXTError = $_ -replace "$Domain : "
     }
     try{
         #get SRV records
-        $SRV_Query = Resolve-DnsName -Type SRV -Name "_autodiscover._tcp.$Domain" -DnsOnly | Select-Object -Property NameTarget
+        $SRV_Query = Resolve-DnsName -Type SRV -Name "_autodiscover._tcp.$Domain" | Select-Object -Property NameTarget
 
         if($SRV_Query){
             foreach($SRV_Result in $SRV_Query){
@@ -96,7 +96,7 @@ $Csv.Domain | ForEach-Object {
         }        
     }
     catch{
-        $Obj.SRVError  = $_    
+        $Obj.SRVError  = $_ -replace "_autodiscover._tcp.$Domain : "
     }
     $Result_list += $Obj
 }
@@ -112,46 +112,46 @@ $Report_items = @(
 $body = @"
 <h3>Domains with unconfigured MX</h3>
 $($Result_list | Where-Object {$_.MXConfiguration -eq 'Not Configured'} | 
-                 ForEach-Object -Begin   {'<ol>'} `
+                 ForEach-Object -Begin   {'<ol>' } `
                                 -Process {"<li><b>$($_.Domain)</b></li>"} `
                                 -End     {'</ol>'})
 <h3>Domains with different MX present</h3>
 $($Result_list | Where-Object {$_.MXConfiguration -eq 'Different MX Present'} |
-                 ForEach-Object -Begin   {'<ol>'} `
+                 ForEach-Object -Begin   {'<ol>' } `
                                 -Process {"<li><b>$($_.Domain)</b></li>"} `
                                 -End     {'</ol>'})
 <h3>Domains with partial MX configuration</h3>
 $($Result_list | Where-Object {$_.MXConfiguration -eq 'Partially Configured'} |
-                 ForEach-Object -Begin   {'<ol>'} `
+                 ForEach-Object -Begin   {'<ol>' } `
                                 -Process {"<li><b>$($_.Domain)</b></li>"} `
                                 -End     {'</ol>'})
 <h3>Domains with MX record errors</h3>
-$($Result_list | Where-Object {$_.MXError -ne 'N/A'} |
-                 ForEach-Object -Begin   {'<ol>'} `
-                                -Process {"<li><b>$($_.Domain):</b> <b style='color:red;'>$($_.MXError)</b></li>"} `
+$($Result_list | Where-Object {$_.MXError -ne 'N/A' -and $_.MXError -ne 'This operation returned because the timeout period expired'} | Sort-Object -Property MXError |
+                 ForEach-Object -Begin   {'<ol>' } `
+                                -Process {"<li><b>$($_.Domain)</b>: <b style='color:red;'>$($_.MXError)</b></li>"} `
                                 -End     {'</ol>'})
 <h3>Domains with unconfigured SRV</h3>
 $($Result_list | Where-Object {$_.SRVConfigured -eq $false} |
-                 ForEach-Object -Begin   {'<ol>'} `
+                 ForEach-Object -Begin   {'<ol>' } `
                                 -Process {"<li><b>$($_.Domain)</b></li>"} `
                                 -End     {'</ol>'})
 <h3>Domains with SRV record errors</h3>
-$($Result_list | Where-Object {$_.SRVError -ne 'N/A'} |
-                 ForEach-Object -Begin   {'<ol>'} `
-                                -Process {"<li><b>$($_.Domain):</b> <b style='color:red;'>$($_.SRVError)</b></li>"} `
+$($Result_list | Where-Object {$_.SRVError -ne 'N/A' -and $_.SRVError -ne 'This operation returned because the timeout period expired'} | Sort-Object -Property SRVError |
+                 ForEach-Object -Begin   {'<ol>' } `
+                                -Process {"<li><b><b>$($_.Domain)</b>:</b> <b style='color:red;'>$($_.SRVError)</b></li>"} `
                                 -End     {'</ol>'})
 <h3>Domains with unconfigured SPF</h3>
 $($Result_list | Where-Object {$_.SPFConfigured -eq $false} |
-                 ForEach-Object -Begin   {'<ol>'} `
+                 ForEach-Object -Begin   {'<ol>' } `
                                 -Process {"<li><b>$($_.Domain)</b></li>"} `
                                 -End     {'</ol>'})
 <h3>Domains with TXT record errors</h3>
-$($Result_list | Where-Object {$_.TXTError -ne 'N/A'} | Sort-Object -Property TXTError |
-                 ForEach-Object -Begin   {'<ol>'} `
-                                -Process {"<li><b>$($_.Domain):</b> <b style='color:red;'>$($_.TXTError)</b></li>"} `
+$($Result_list | Where-Object {$_.TXTError -ne 'N/A' -and $_.TXTError -ne 'This operation returned because the timeout period expired'} | Sort-Object -Property TXTError |
+                 ForEach-Object -Begin   {'<ol>' } `
+                                -Process {"<li><b><b>$($_.Domain)</b>:</b> <b style='color:red;'>$($_.TXTError)</b></li>"} `
                                 -End     {'</ol>'})
 "@
 
-#Write-Output $Result_list
+write-Output $Result_list
 
 Send-MailMessage @MailSettings -BodyAsHtml "$Style $body"
