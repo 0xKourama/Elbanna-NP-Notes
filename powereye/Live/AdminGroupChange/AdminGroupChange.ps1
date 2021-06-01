6a30226010972a5eb6d8cdbc5754d169dcb6784a
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-06-01 16:34:21 +0200

    +1

--- powereye/Live/AdminGroupChange/AdminGroupChange.ps1
+++ powereye/Live/AdminGroupChange/AdminGroupChange.ps1
@@ -4,7 +4,7 @@ Invoke-Expression -Command (Get-Content -Path 'Mail-Settings.txt' -Raw)
 Invoke-Expression -Command (Get-Content -Path 'HTML-Layout.txt'   -Raw)
 
 $Admin_Security_Groups = @(
-	'Administrators'
+    'Administrators'
 	'Authorized Personnel'
 	'Compliance Management'
 	'Delegated Setup'
@@ -32,14 +32,14 @@ $Admin_Security_Groups = @(
 	'Server Operators'
 	'UM Management'
 	'View-Only Organization Management'
-	'WP-Services-Import_Export_MBXs'
+	'Import-Export-Mailboxes'
 	'WP-Support-L1'
 	'WP-Support-L2'
 )
 
 $Script = {
     #supress errors, only error is when there are no logs matching the criteria
-    #$ErrorActionPreference = 'SilentlyContinue'
+    $ErrorActionPreference = 'SilentlyContinue'
 
     $EventID1 = 4728
     $EventID2 = 4729
@@ -55,23 +55,23 @@ $Script = {
     )
 
     if(!(Test-Path -Path $XML_Path1)){
-        $Events += Get-WinEvent -FilterHashtable @{LogName = 'ForwardedEvents'; ID = $EventID1} | Select-Object -Property $Collected_Properties
+        $Events += Get-WinEvent -FilterHashtable @{LogName = 'Security'; ID = $EventID1} | Select-Object -Property $Collected_Properties
     }
     else{
-        $Events += Get-WinEvent -FilterHashtable @{LogName = 'ForwardedEvents'; ID = $EventID1; StartTime = Import-Clixml -Path $XML_Path1} |
+        $Events += Get-WinEvent -FilterHashtable @{LogName = 'Security'; ID = $EventID1; StartTime = Import-Clixml -Path $XML_Path1} |
                     Select-Object -Property $Collected_Properties
     }
 
     if(!(Test-Path -Path $XML_Path2)){
-        $Events += Get-WinEvent -FilterHashtable @{LogName = 'ForwardedEvents'; ID = $EventID2} | Select-Object -Property $Collected_Properties
+        $Events += Get-WinEvent -FilterHashtable @{LogName = 'Security'; ID = $EventID2} | Select-Object -Property $Collected_Properties
     }
     else{
-        $Events += Get-WinEvent -FilterHashtable @{LogName = 'ForwardedEvents'; ID = $EventID2; StartTime = Import-Clixml -Path $XML_Path2} |
+        $Events += Get-WinEvent -FilterHashtable @{LogName = 'Security'; ID = $EventID2; StartTime = Import-Clixml -Path $XML_Path2} |
                   Select-Object -Property $Collected_Properties
     }
+
     Get-Date | Export-Clixml -Path $XML_Path1
     Get-Date | Export-Clixml -Path $XML_Path2
-
     $Results = @()
 
     foreach($Event in $Events){
@@ -80,7 +80,7 @@ $Script = {
         $Subject_Part = ($Message | Select-String -Pattern '^Subject:\s+$' -Context 0,2).Context.PostContext
         $Member_Part  = ($Message | Select-String -Pattern '^Member:\s+$'  -Context 0,2).Context.PostContext
         $Group_Part   = ($Message | Select-String -Pattern '^Group:\s+$'   -Context 0,2).Context.PostContext
-
+        
         $Results += [PSCustomObject][Ordered]@{
             Date          = $Event.TimeCreated
             Action        = if($Event.Id -eq 4728){'Addition'}else{'Removal'}
@@ -92,7 +92,9 @@ $Script = {
     $Results
 }
 
-$Results = Invoke-Command -ComputerName frank-eventlog -ScriptBlock $Script
+$Results = Invoke-Command -ComputerName (Return-OnlineComputers -ComputerNames (Get-ADDomainController -Filter *).Name) -ScriptBlock $Script | 
+           Select-Object -Property Date, @{n='ComputerName';e={$_.PSComputerName}}, Action, Group, SourceAccount, @{n='TagetAccount';e={$_.TargetAccount -replace ',\w{2}=.*' -replace 'CN='}}
+
 $NormalResults = $Results | Where-Object {$Admin_Security_Groups -notcontains $_.Group}
 $AdminResults  = $Results | Where-Object {$Admin_Security_Groups -contains    $_.Group}
 
@@ -114,3 +116,7 @@ if($Results){
     Send-MailMessage @MailSettings -BodyAsHtml "$Style $Body"
     Clear-Variable -Name Body
 }
+
+#testing
+#$Admin_Security_Groups | %{remove-ADGroupMember -Identity (Get-ADGroup -Identity $_) -Members unauthtest -Confirm:$false}
+#$Admin_Security_Groups | %{Add-ADGroupMember -Identity (Get-ADGroup -Identity $_) -Members unauthtest}
