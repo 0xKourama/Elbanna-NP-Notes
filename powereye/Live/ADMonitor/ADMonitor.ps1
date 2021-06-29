db246e0d91eee2ce1bae6481922f2f571ae0fc68
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-06-29 16:12:49 +0200

    +1

--- powereye/Live/ADMonitor/ADMonitor.ps1
+++ powereye/Live/ADMonitor/ADMonitor.ps1
@@ -9,8 +9,17 @@ Invoke-Expression -Command (Get-Content -Path 'HTML-Layout.txt'   -Raw)
     }
 
     $EventList = @(
-		[Event]@{ID=4720; Title='User created'}
         [Event]@{ID=4726; Title='User deleted'}
+        [Event]@{ID=4743; Title='Computer account was deleted'}
+        [Event]@{ID=4734; Title='Local security group deleted'}
+        [Event]@{ID=4728; Title='Global security group member added'}
+        [Event]@{ID=4758; Title='Universal security group deleted'}
+        [Event]@{ID=4748; Title='Local distribution group deleted'}
+        [Event]@{ID=4753; Title='Global distribution group deleted'}
+        [Event]@{ID=5141; Title='ADObject deleted'}
+
+		[Event]@{ID=4720; Title='User created'}
+        #[Event]@{ID=4726; Title='User deleted'}
         [Event]@{ID=4722; Title='User enabled'}
         [Event]@{ID=4725; Title='User disabled'}
         [Event]@{ID=4723; Title='User changed his own password'}
@@ -19,53 +28,54 @@ Invoke-Expression -Command (Get-Content -Path 'HTML-Layout.txt'   -Raw)
         [Event]@{ID=4740; Title='User locked out'}
         [Event]@{ID=4767; Title='User unlocked'}
         [Event]@{ID=4781; Title='User name changed'}
+        [Event]@{ID=4763; Title='Universal distribution group deleted'}
 
         [Event]@{ID=4741; Title='Computer account created'}
         [Event]@{ID=4742; Title='Computer account changed'}
-        [Event]@{ID=4743; Title='Computer account was deleted'}
+        #[Event]@{ID=4743; Title='Computer account was deleted'}
 
         [Event]@{ID=4764; Title='A Group type was changed'}
 
         [Event]@{ID=4731; Title='Local security group created'}
         ##[Event]@{ID=4735; Title='Local security group changed'}
-        [Event]@{ID=4734; Title='Local security group deleted'}
+        #[Event]@{ID=4734; Title='Local security group deleted'}
         [Event]@{ID=4732; Title='Local security group member added'}
         [Event]@{ID=4733; Title='Local security group member removed'}
 
         [Event]@{ID=4727; Title='Global security group created'}
         [Event]@{ID=4737; Title='Global security group changed'}
         [Event]@{ID=4730; Title='Global security group deleted'}
-        [Event]@{ID=4728; Title='Global security group member added'}
+        #[Event]@{ID=4728; Title='Global security group member added'}
         [Event]@{ID=4729; Title='Global security group member removed'}
 
         [Event]@{ID=4754; Title='Universal security group created'}
         [Event]@{ID=4755; Title='Universal security group changed'}
-        [Event]@{ID=4758; Title='Universal security group deleted'}
+        #[Event]@{ID=4758; Title='Universal security group deleted'}
         [Event]@{ID=4756; Title='Universal security group member added'}
         [Event]@{ID=4757; Title='Universal security group member removed'}
 
         [Event]@{ID=4744; Title='Local distribution group created'}
         [Event]@{ID=4745; Title='Local distribution group changed'}
-        [Event]@{ID=4748; Title='Local distribution group deleted'}
+        #[Event]@{ID=4748; Title='Local distribution group deleted'}
         [Event]@{ID=4746; Title='Local distribution group member added'}
         [Event]@{ID=4747; Title='Local distribution group member removed'}
 
         [Event]@{ID=4749; Title='Global distribution group created'}
         [Event]@{ID=4750; Title='Global distribution group changed'}
-        [Event]@{ID=4753; Title='Global distribution group deleted'}
+        #[Event]@{ID=4753; Title='Global distribution group deleted'}
         [Event]@{ID=4751; Title='Global distribution group member added'}
         [Event]@{ID=4752; Title='Global distribution group member removed'}
 
         [Event]@{ID=4759; Title='Universal distribution group created'}
         [Event]@{ID=4760; Title='Universal distribution group changed'}
-        [Event]@{ID=4763; Title='Universal distribution group deleted'}
+        #[Event]@{ID=4763; Title='Universal distribution group deleted'}
         [Event]@{ID=4761; Title='Universal distribution group member added'}
         [Event]@{ID=4762; Title='Universal distribution group member removed'}
 
         [Event]@{ID=5137; Title='ADobject Created'}
         #[Event]@{ID=5136; Title='ADObject Modified'}
         [Event]@{ID=5139; Title='ADObject Moved'}
-        [Event]@{ID=5141; Title='ADObject deleted'}
+        #[Event]@{ID=5141; Title='ADObject deleted'}
         [Event]@{ID=5138; Title='ADObject Undeleted'}
 
         [Event]@{ID=4794; Title='Attempt to set DSRM Administrator password'}
