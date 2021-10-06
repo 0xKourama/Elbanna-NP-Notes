55942a6696e28a6b82a2d4eb08a203edda586cf0
mohammadgabr360 <mohammad.magdy.gabr@hotmail.com>
2021-10-06 12:58:59 +0200

    +1

--- toolkit/Get-SoftwareInformation.ps1
+++ toolkit/Get-SoftwareInformation.ps1
@@ -1,4 +1,4 @@
 ﻿Get-ItemProperty -Path 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' |
 Where-Object {$_.Displayname} |
-Select-Object -Property 'DisplayName','DisplayVersion','Publisher','InstallDate' |
+Select-Object -Property 'DisplayName','DisplayVersion','Publisher','InstallDate', 'UninstallString' |
 Sort-Object -Property DisplayName
