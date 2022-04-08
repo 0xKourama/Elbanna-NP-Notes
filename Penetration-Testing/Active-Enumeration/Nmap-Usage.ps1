d24f8b6f1d7b77ec338dd91d8ee8720dcaf5b48f
MohammadGabr360 <Mohammad.Magdy.Gabr@hotmail.com>
2022-04-08 16:40:26 +0200

    +1

--- Penetration-Testing/Active-Enumeration/Nmap-Usage.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host 'Nmap usage:'
-Write-Host -NoNewline 'nmap '
-Write-Host -NoNewline -ForegroundColor Green '<RHOST> '
-Write-Host -NoNewline -ForegroundColor Cyan '-sC [Default Scripts] '
-Write-Host -NoNewline -ForegroundColor Cyan '-sV [Service Detection] '
-Write-Host -NoNewline -ForegroundColor Cyan '-p- [All Ports] '
-Write-Host -NoNewline '-oN '
-Write-Host -NoNewline -ForegroundColor Green '[Output File]'
